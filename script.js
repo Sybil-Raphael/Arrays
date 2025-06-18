43686522e8ea3a536516e01b42964ce57432d7e6
Sybil Raphael <156949200+Sybil-Raphael@users.noreply.github.com>
2025-06-18 07:35:55 +0000

    Refactor movie selection logic and improve movie list display

--- script.js
+++ script.js
@@ -3,17 +3,44 @@ document.getElementById("genre").addEventListener("change", function() {
   // Get the selected genre
   const selectedGenre = document.getElementById("genre").value;
 
-  // Add movies to the movieList based on the selected genre
+  // Create a variable to hold the list of movies for the selected genre
+  let movieList = [];
+
+  // Push movie titles directly to movieList based on the selected genre
   if (selectedGenre === "Comedy") {
-    console.log("You selected 'Comedy'");
+    // Comedy movies
+    movieList.push("The Mask", "Superbad", "Dumb and Dumber", "Home Alone");
   } else if (selectedGenre === "Action") {
-    console.log("You selected 'Action'");
+    // Action movies
+    movieList.push("Die Hard","Mad Max: Fury Road","John Wick","The Dark Knight");
   } else if (selectedGenre === "Drama") {
-    console.log("You selected 'Drama'");
+    // Drama movies
+    movieList.push("Forrest Gump","The Shawshank Redemption","The Godfather","A Beautiful Mind");
   } else if (selectedGenre === "Sci-Fi") {
-    console.log("You selected 'Sci-Fi'");
+    // Sci-Fi movies
+    movieList.push("Inception","The Matrix","Interstellar","Star Wars");
   }
 
   // Display the list of movies on the page
-  document.getElementById("movieRecommendations").innerText = `Enjoy: !`;
+  document.getElementById("movieRecommendations").innerText = `Enjoy: ${movieList.join(", ")}`;
 });
+  let movieList = [];
+
+  // Assign the correct movie array to movieList based on the selected genre
+  if (selectedGenre === "Comedy") {
+    // If Comedy is selected, use comedyMovies array
+    movieList = comedyMovies;
+  } else if (selectedGenre === "Action") {
+    // If Action is selected, use actionMovies array
+    movieList = actionMovies;
+  } else if (selectedGenre === "Drama") {
+    // If Drama is selected, use dramaMovies array
+    movieList = dramaMovies;
+  } else if (selectedGenre === "Sci-Fi") {
+    // If Sci-Fi is selected, use sciFiMovies array
+    movieList = sciFiMovies;
+  }
+
+  // Display the list of movies on the page
+  document.getElementById("movieRecommendations").innerText = `Enjoy: ${movieList.join(", ")}`;
+
